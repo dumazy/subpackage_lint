fb8e1132f4214f5d9a5edce5b7421374b3381c0a
Fré Dumazy <dumazy@gmail.com>
2023-05-26 23:21:17 +0200

    Add more examples to fix

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,4 +1,6 @@
-import 'package:example/feature/feature.dart';
+// import 'package:example/feature/src/code.dart'; // invalid absolute import
+// import './feature/src/code.dart'; // invalid relative import
+import 'package:example/feature/feature.dart'; // correct
 
 void main() {
   exampleFunction();
