02024bc6002d52b949c043d676bde978ecd060cb
Fré Dumazy <dumazy@gmail.com>
2023-05-29 21:24:44 +0200

    Reset example project

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:example/feature/src/code.dart'; // invalid absolute import
+// import 'package:example/feature/src/code.dart'; // invalid absolute import
 // import './feature/src/code.dart'; // invalid relative import
 import 'package:example/feature/feature.dart'; // correct
 
